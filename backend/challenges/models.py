e8990db1fa3e5a075da7a37c82773daf47436a80
Shweta-pi <shwetasin9795@gmail.com>
2026-09-24 22:02:44 +0530

    Challenges

--- backend/challenges/models.py
+++ backend/challenges/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Challenge(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    difficulty = models.CharField(max_length=50)
+    points = models.IntegerField(default=10)
+
+    def __str__(self):
+        return self.title
